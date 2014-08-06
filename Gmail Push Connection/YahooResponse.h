bb6db39736f7417d9bad5f9a7ba4a4a3c843cbd7
alexclp <alexandru.clapa@gmail.com>
2014-08-07 02:49:58 +0300

    class for yahoo response

---
+++ Gmail Push Connection/YahooResponse.h
@@ -0,0 +1,13 @@
+//
+//  YahooResponse.h
+//  Token Auths
+//
+//  Created by Alexandru Clapa on 07.08.2014.
+//  Copyright (c) 2014 Alexandru Clapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YahooResponse : NSObject
+
+@end
---
+++ Gmail Push Connection/YahooResponse.m
@@ -0,0 +1,13 @@
+//
+//  YahooResponse.m
+//  Token Auths
+//
+//  Created by Alexandru Clapa on 07.08.2014.
+//  Copyright (c) 2014 Alexandru Clapa. All rights reserved.
+//
+
+#import "YahooResponse.h"
+
+@implementation YahooResponse
+
+@end
