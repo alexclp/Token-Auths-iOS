b5ac3b9040bcfcb211eee1b06a33a67d9cb98d10
alexclp <alexandru.clapa@gmail.com>
2014-08-06 23:08:19 +0300

    added networking class

---
+++ Gmail Push Connection/Networking.h
@@ -0,0 +1,14 @@
+//
+//  Networking.h
+//  Token Auths
+//
+//  Created by Alexandru Clapa on 06.08.2014.
+//  Copyright (c) 2014 Alexandru Clapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include "NSDictionary_JSONExtensions.h"
+
+@interface Networking : NSObject
+
+@end
---
+++ Gmail Push Connection/Networking.m
@@ -0,0 +1,13 @@
+//
+//  Networking.m
+//  Token Auths
+//
+//  Created by Alexandru Clapa on 06.08.2014.
+//  Copyright (c) 2014 Alexandru Clapa. All rights reserved.
+//
+
+#import "Networking.h"
+
+@implementation Networking
+
+@end
