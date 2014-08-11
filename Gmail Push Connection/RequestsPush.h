a91f25b6edd66b849ec47a7774f98468bf8db179
alexclp <alexandru.clapa@gmail.com>
2014-08-11 17:05:04 +0300

    added new class

---
+++ Gmail Push Connection/RequestsPush.h
@@ -0,0 +1,13 @@
+//
+//  RequestsPush.h
+//  Token Auths
+//
+//  Created by Alexandru Clapa on 11.08.2014.
+//  Copyright (c) 2014 Alexandru Clapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RequestsPush : NSObject
+
+@end
---
+++ Gmail Push Connection/RequestsPush.m
@@ -0,0 +1,13 @@
+//
+//  RequestsPush.m
+//  Token Auths
+//
+//  Created by Alexandru Clapa on 11.08.2014.
+//  Copyright (c) 2014 Alexandru Clapa. All rights reserved.
+//
+
+#import "RequestsPush.h"
+
+@implementation RequestsPush
+
+@end
